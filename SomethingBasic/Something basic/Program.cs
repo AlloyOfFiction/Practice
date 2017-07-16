156eca7578a236399e5aa75a6e127e08db9028c0
AlloyOfFiction <alloyoffiction@gmail.com>
2017-07-16 03:05:06 +0200

    Adding ChatOption to program
    Refactored chat options into an enum for better code reading.

--- SomethingBasic/Something basic/Program.cs
+++ SomethingBasic/Something basic/Program.cs
@@ -14,50 +14,81 @@ namespace Something_basic
     public static int paranoiaA = 5;
     public static int suspiciousA = 5;
 
-    static string choice = "D";
+
+        public enum ChatOption
+        {
+            Invalid,
+            A,
+            B,
+            C
+        }
 
         
        
 
         static void Main(string[] args)
         {
-           effectTrust();
-           trustResult();
+           EffectTrust();
+           TrustResult();
 
 
             System.Console.ReadKey();
         }
 
 
-        private static void effectTrust()
+        private static void EffectTrust()
         {
             System.Console.WriteLine ("1. Option A");
             System.Console.WriteLine("2. Option B");
             System.Console.WriteLine("3. Option C");
 
-            choice = System.Console.ReadLine();
+            string choice = System.Console.ReadLine();
+            ChatOption choiceCheck = ValidChoice(choice);
 
-            if ((choice == "A"))
+            if (choiceCheck == ChatOption.Invalid)
+            {
+                // Do nothing
+            }
+            else if (choiceCheck == ChatOption.A)
             {
             System.Console.WriteLine ("did it work?");
             trustA = trustA + 1;
             }
-
-            if ((choice == "B"))
+            else if (choiceCheck == ChatOption.B)
             {
             System.Console.WriteLine ("Other option");
             paranoiaA = paranoiaA + 2;
 
             }
-
-            else
+            else if (choiceCheck == ChatOption.C)
             {
             System.Console.WriteLine ("poor choice");
             trustA = trustA - 1;
             }
 
         }
-        private static void trustResult()
+
+        private static ChatOption ValidChoice(string choice)
+        {
+            if (choice == "A")
+            {
+                return ChatOption.A;
+            }
+            else if (choice == "B")
+            {
+                return ChatOption.B;
+            }
+            else if (choice == "C")
+            {
+                return ChatOption.C;
+            }
+            else
+            {
+                return ChatOption.Invalid;
+            }
+        }
+
+        private static void TrustResult()
         {
             Convert.ToString(trustA);
             Convert.ToString(paranoiaA);
